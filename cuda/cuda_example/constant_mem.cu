3739a2d251baa59a6f92ab70b64510d245f5a0da
ycliang <ycliang@cs.nctu.edu.tw>
2019-05-05 22:00:38 +0800

    Understand how constant memory works

--- cuda/cuda_example/constant_mem.cu
+++ cuda/cuda_example/constant_mem.cu
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__device__ __constant__ int test_arr_d[5] = {1,2,3,4,5};
-__device__ __constant__ int a = 10;
+__constant__ int test_arr_d[5];
+__constant__ int a;
+
+
+__global__ void print()
+{
+	int id = threadIdx.x;
+	printf("%d: %d\n", id, test_arr_d[id]);
+	__syncthreads();
+}
 
 int main()
 {
-//	__constant__ int test_arr_d[5];
-//	cudaMalloc((void**)&test_arr_d, sizeof(int)*5);
 	int test_arr_h[5] = {1, 2, 3, 4, 5};
-	int a_h = 10;
-//	cudaError_t result = cudaMemcpyToSymbol(test_arr_d, &test_arr_h, 5*sizeof(int), 0, cudaMemcpyHostToDevice);
-//	cudaError_t result1 = cudaMemcpyToSymbol(a, &a_h, sizeof(int), 0, cudaMemcpyHostToDevice);
+	cudaError_t result = cudaMemcpyToSymbolAsync(test_arr_d, &test_arr_h, 5*sizeof(int), 0, cudaMemcpyHostToDevice);
+	print<<<1,5>>>();
 	cudaDeviceSynchronize();
-	puts("haha");
-//	cudaFree(test_arr_d);
 	return 0;
 }
